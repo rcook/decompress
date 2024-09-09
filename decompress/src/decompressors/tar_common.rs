899bfe1e83a5ece3128d3523f2488f802545ccd7
Richard Cook <rcook@rcook.org>
2024-09-09 11:55:30 -0700

    Properly handle symlinks in tarballs

--- decompress/src/decompressors/tar_common.rs
+++ decompress/src/decompressors/tar_common.rs
@@ -6,7 +6,7 @@ use std::{
 };
 
 use crate::{DecompressError, ExtractOpts};
-use tar::Archive;
+use tar::{Archive, EntryType};
 
 pub fn tar_list(out: &mut Archive<Box<dyn Read>>) -> Result<Vec<String>, DecompressError> {
     Ok(out
@@ -32,7 +32,7 @@ pub fn tar_extract(
 
     // alternative impl: just unpack, and then mv everything back X levels
     for entry in out.entries()? {
-        let entry = entry?;
+        let mut entry = entry?;
         let filepath = entry.path()?;
 
         // strip prefixed components. this can be 0 parts, in which case strip does not happen.
@@ -53,30 +53,43 @@ pub fn tar_extract(
 
         let outpath: Cow<'_, Path> = (opts.map)(outpath.as_path());
 
-        if entry.header().entry_type() != tar::EntryType::Directory {
-            if let Some(p) = outpath.parent() {
-                if !p.exists() {
-                    fs::create_dir_all(p)?;
+        match entry.header().entry_type() {
+            EntryType::Directory => {}
+            EntryType::Regular => {
+                if let Some(p) = outpath.parent() {
+                    if !p.exists() {
+                        fs::create_dir_all(p)?;
+                    }
+                }
+
+                let mut outfile = fs::File::create(&outpath)?;
+
+                #[cfg(unix)]
+                let h = entry.header().mode();
+
+                io::copy(&mut BufReader::new(entry), &mut outfile)?;
+                files.push(outpath.to_string_lossy().to_string());
+
+                #[cfg(unix)]
+                {
+                    use crate::decompressors::utils::normalize_mode;
+                    use std::os::unix::fs::PermissionsExt;
+                    if let Ok(mode) = h {
+                        let mode = normalize_mode(mode);
+                        fs::set_permissions(&outpath, fs::Permissions::from_mode(mode))?;
+                    }
                 }
             }
-
-            let mut outfile = fs::File::create(&outpath)?;
-
-            #[cfg(unix)]
-            let h = entry.header().mode();
-
-            io::copy(&mut BufReader::new(entry), &mut outfile)?;
-            files.push(outpath.to_string_lossy().to_string());
-
-            #[cfg(unix)]
-            {
-                use crate::decompressors::utils::normalize_mode;
-                use std::os::unix::fs::PermissionsExt;
-                if let Ok(mode) = h {
-                    let mode = normalize_mode(mode);
-                    fs::set_permissions(&outpath, fs::Permissions::from_mode(mode))?;
+            EntryType::Symlink => {
+                if let Some(p) = outpath.parent() {
+                    if !p.exists() {
+                        fs::create_dir_all(p)?;
+                    }
                 }
+
+                entry.unpack(&outpath)?;
             }
+            e => todo!("Unsupported entry type {e:?}"),
         }
     }
     Ok(files)
